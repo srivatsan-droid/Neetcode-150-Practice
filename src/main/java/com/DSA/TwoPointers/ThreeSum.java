20251b9fda70b0763f759cf9bd1b487c59de8086
Srivatsan V <sriramsrivatsan2001@gmail.com>
2026-07-08 22:21:47 +0530

    Three Sum Optimized Solution Updated

--- src/main/java/com/DSA/TwoPointers/ThreeSum.java
+++ src/main/java/com/DSA/TwoPointers/ThreeSum.java
@@ -18,9 +18,41 @@ public class ThreeSum {
         }
         return new ArrayList<>(set);
     }
+    public static List<List<Integer>> threeSumOptimized(int nums[]) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 0;i < nums.length-2;i++) {
+            int j = i + 1;
+            int k = nums.length-1;
+            if(i > j && nums[i] == nums[i-1]) {
+                continue;
+            }
+            while(j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum == 0) {
+                    ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    while(j < k && nums[j] == nums[j-1]) {
+                        j++;
+                    }
+                    while(j < k && nums[k] == nums[k-1]) {
+                        continue;
+                    }
+                    j++;
+                    k--;
+                }
+                else if(sum < 0) {
+                    j++;
+                }
+                else {
+                    k--;
+                }
+            }
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         int nums[] = {-1,0,1,2,-1,-4};
-        List<List<Integer>> ans = threeSum(nums);
+        List<List<Integer>> ans = threeSumOptimized(nums);
         for(int i = 0;i < ans.size();i++) {
             System.out.println(ans.get(i));
         }
