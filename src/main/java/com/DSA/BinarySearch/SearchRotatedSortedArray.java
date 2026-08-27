37b283983fc2324a52471c92bf2370022709267f
Srivatsan V <sriramsrivatsan2001@gmail.com>
2026-08-27 14:02:33 +0530

    Problem 4 - Search in Rotated Sorted Array

--- src/main/java/com/DSA/BinarySearch/SearchRotatedSortedArray.java
+++ src/main/java/com/DSA/BinarySearch/SearchRotatedSortedArray.java
@@ -10,41 +10,31 @@ public class SearchRotatedSortedArray {
         }
         return -1;
     }
-    public static int searchRotatedArray(int[] nums, int target) {
-
-        int low = 0;
-        int high = nums.length - 1;
-
-        while (low <= high) {
-
+    public static int searchRotatedArray(int nums[],int target) {
+        int low = 0,high = nums.length - 1;
+        while(low <= high) {
             int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target)
+            if(nums[mid] == target) {
                 return mid;
-
-            // Left half is sorted
-            if (nums[low] <= nums[mid]) {
-
-                if (target >= nums[low] && target < nums[mid]) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-
             }
-            // Right half is sorted
-            else {
-
-                if (target > nums[mid] && target <= nums[high]) {
-                    low = mid + 1;
-                } else {
+            if(nums[low] <= nums[mid]) {
+                if(target >= nums[low] && target <= nums[mid]) {
                     high = mid - 1;
                 }
-
+                else {
+                    low = mid + 1;
+                }
+            }
+            else {
+                if(target >= nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid -1;
+                }
             }
         }
-
-        return -1;
+        return low;
     }
     public static void main(String[] args) {
         int nums[] = {4,5,6,7,0,1,2};
