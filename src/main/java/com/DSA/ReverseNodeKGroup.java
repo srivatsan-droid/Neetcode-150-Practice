84a32057470586bb8dea653ce24c254fa459a0db
Srivatsan V <sriramsrivatsan2001@gmail.com>
2026-09-09 10:14:41 +0530

    Reverse Node K Group

---
+++ src/main/java/com/DSA/ReverseNodeKGroup.java
@@ -0,0 +1,49 @@
+package com.DSA;
+
+public class ReverseNodeKGroup {
+    public static ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null) return null;
+
+        ListNode tail = head;
+        for (int i = 0; i < k; i++) {
+            if (tail == null) return head;
+            tail = tail.next;
+        }
+
+        ListNode newHead = reverse(head, tail);
+        head.next = reverseKGroup(tail, k);
+        return newHead;
+    }
+
+    private static ListNode reverse(ListNode cur, ListNode end) {
+        ListNode prev = null;
+        while (cur != end) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+        while(temp != null) {
+            System.out.println(temp.val);
+            if(temp.next != null) {
+                System.out.println("-->");
+            }
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        int k = 2;
+        head = reverseKGroup(head,k);
+        printList(head);
+    }
+}
